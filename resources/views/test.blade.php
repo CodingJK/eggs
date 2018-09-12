e2fb9e2189ac9892111a9310ad123fdd23ade84f
eleanor <yyjeleanor@gmail.com>
2018-09-12 18:03:53 -0400

    change test page

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -2,23 +2,37 @@
 
 @section('content')
 	<script>
-		$(window).ready(function(){
-			$(".nav_parent").css('background-color','white');
-			$("#nav").css('background-color','white');
-			// $(".nav-justified a").css('border-color','white');
-			// $(".nav-justified a").css('color','black');
-			// $(".nav-justified a").mouseenter( function(){
-			// 	$(this).css('border-color','#ffcd32');
-			// } ).mouseleave(  function(){
-			// 	$(this).css('border-color','white');
-			// } );
-			$(".apps_link").css('color','#ffcd32');
-
-
-		});
+		 $(window).ready(function(){
+                $('.links a').css('border','2px solid #ffbc27');  
+                $("#nav").css('background-color','#fbbc27');
+                $("#nav").css('border-color','transparent');
+                
+                $('.links a').hover(function(){
+                    $(this).css('border','2px solid white');
+                    $(this).css('color','white');
+                },function(){
+                    $(this).css('border','2px solid #fbbb27'); 
+                    $(this).css('color','black');
+                    
+                });   
+               
+                
+                
+            });
 	</script> 
 
 <body>
+    <div id='big_screen'>
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <a href="/Recipes">
+                    <div class='row Header11'>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
     <div class="main container" style="margin-top: 200px;">
         <div class='col-xs-12'>
             @if (session()->has('status'))
@@ -61,71 +75,14 @@
                             required>
                     </div>
                 </div>
-                <div class="form-group row">
-                    <label for="address" class="col-xs-2 col-form-label">Address</label>
-                    <div class="col-xs-10">
-                        <input type="text" class="form-control form-control-sm" id="address" name='address' value="{{old('address')}}" required>
-                    </div>
-                </div>
-            
-                <div class="form-group row ">
-                    <label for="city" class="col-xs-2 col-form-label">City</label>
-                    <div class="col-xs-4">
-                        <input type="text" class="form-control form-control-sm" id="city" name='city' value="{{old('city')}}" required>
-                    </div>
-                    <label for="province" class="col-xs-2 col-form-label">Province</label>
-                    <div class="col-xs-4">
-                        <select class="form-control" id='province' name='province' value="{{old('province')}}">
-                            <option value="AB">Alberta</option>
-                            <option value="BC">British Columbia</option>
-                            <option value="MB">Manitoba</option>
-                            <option value="NB">New Brunswick</option>
-                            <option value="NL">Newfoundland</option>
-                            <option value="NT">Northwest Territories</option>
-                            <option value="NS">Nova Scotia</option>
-                            <option value="NU">Nunavut</option>
-                            <option value="ON">Ontario</option>
-                            <option value="PE">Prince Edward Island</option>
-                            <option value="QC">Quebec</option>
-                            <option value="SK">Saskatchewan</option>
-                            <option value="TY">Yukon Territory</option>
-                            <option value="Other">Other</option>
-                        </select>
-                    </div>
-                </div>
-            
-                <div class="form-group row">
-                    <label for="postalcode" class="col-xs-2 col-form-label">Postal Code</label>
-                    <div class="col-xs-4">
-                        <input type="text" class="form-control form-control-sm" id="postalcode" name='postalcode' value="{{old('postalcode')}}" required>
-                    </div>
-                    <label for="country" class="col-xs-2 col-form-label">Country</label>
-                    <div class="col-xs-4">
-                        <input type="text" class="form-control form-control-sm" id="country" name='country' value="{{old('country')}}" required>
-                    </div>
-                </div>
-
-                <div class="form-group row" style='padding:0 15px;'>
-                    <img src="" id='myPic'>
-                </div>
-
-                <div class="form-group row">
-                    <label for="title" class="col-xs-2 col-form-label">Work Title</label>
-                    <div class="col-xs-10">
-                        <input type="text" class="form-control form-control-sm" id="title" name='title' value="{{old('title')}}" required>
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <label for="description" class="col-xs-2 col-form-label">Description</label>
-                    <div class="col-xs-10">
-                        <textarea name="description" id="description" cols="30" rows="10"class="form-control form-control-sm"></textarea>
-                        
-                    </div>
-                </div>
-                
+               
             </form>
+
+
+                
+           
             <label>Upload Image</label>
-                    <form action="/file-upload" class="dropzone" id="myDropzone" style="margin-top:20px;height: 300px;">
+                    <form action="/testsaved" class="dropzone" id="myDropzone" style="margin-top:20px;height: 300px;">
                         <input type="file" name="file" />
                     </form>
              <div class="form-group row" style="margin-top: 20px;">
@@ -168,8 +125,6 @@
             maxFiles:1,//一次性上传的文件数量上限
             maxFilesize: 12, //MB
             acceptedFiles: ".jpg,.gif,.png", //上传的类型
-            
-            
             dictMaxFilesExceeded: "您最多只能上传1个文件！",
             dictResponseError: '文件上传失败!',
             dictInvalidFileType: "你不能上传该类型文件,文件类型只能是*.jpg,*.gif,*.png。",
